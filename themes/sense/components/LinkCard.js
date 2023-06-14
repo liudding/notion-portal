bca09670e2a5b21af6752ad889122c8e04641d6a
liuding <liuding.ld@foxmail.com>
2023-06-14 13:43:49 +0800

    icon

--- themes/sense/components/LinkCard.js
+++ themes/sense/components/LinkCard.js
@@ -1,18 +1,43 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import LinkTag from './LinkTag'
+
+const LinkIcon = ({ link }) => {
+  if (link.pageIcon) {
+    if (link.pageIcon.startsWith('http') || link.pageIcon.startsWith('data:')) {
+      return <Image src={link.pageIcon} width={60} height={60}></Image>
+    }
+
+    return <span className=''>{link.pageIcon}</span>
+  }
+
+  if (link.iconfile) {
+    return <Image src={link.iconfile.url} width={60} height={60}></Image>
+  }
+
+  if (link.iconlink) {
+    return <Image src={link.iconlink} width={60} height={60}></Image>
+  }
+
+  return null
+}
 
 const LinkCard = ({ link }) => {
+  if (link.Name && link.Name.indexOf('Vercel') >= 0) {
+    console.log(link)
+  }
+
   return (
-    <Link href={link.URL || ''} target='_blank' className='grid-item  bg-white rounded-md p-1 bg-white' style={{ breakInside: 'avoid', height: 180, boxShadow: '0 0px 6px 3px rgb(0 0 0 / 0.1), 0 0px 0px -1px rgb(0 0 0 / 0.1)' }} key={link.id}>
+    <Link href={link.URL || ''} target='_blank' className='grid-item  bg-white rounded-md p-2 bg-white' style={{ breakInside: 'avoid', height: 180, boxShadow: '0 0px 6px 3px rgb(0 0 0 / 0.1), 0 0px 0px -1px rgb(0 0 0 / 0.1)' }} key={link.id}>
       <div className='p-1' style={{ height: 60 }}>
-        {link.icon ? <Image src={link.icon} width={60} height={60}></Image> : <div></div>}
+        <LinkIcon link={link}></LinkIcon>
       </div>
 
       <div className='mt-2 p-2 rounded-md' style={{ height: 80 }}>
         <div className='line-clamp-1'>{link.Name}</div>
 
-        <div className='mt-2'>
-          {(link.tags || []).map(tag => <span className='text-xs text-gray-400 px-2 py-1 mr-2 rounded-full border' key={tag}>{tag}</span>)}
+        <div className='mt-2 flex'>
+          {(link.tagItems || []).map(tag => <LinkTag key={tag.name} tag={tag}></LinkTag>)}
         </div>
       </div>
 
---
+++ themes/sense/components/LinkTag.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const tagColors = {
+  gray: 'rgba(120, 119, 116, 1)',
+  brown: 'rgba(159, 107, 83, 1)',
+  orange: 'rgba(217, 115, 13, 1)',
+  yellow: 'rgba(203, 145, 47, 1)',
+  green: 'rgba(68, 131, 97, 1)',
+  purple: 'rgba(144, 101, 176, 1)',
+  pink: 'rgba(193, 76, 138, 1)',
+  red: 'rgba(212, 76, 71, 1)'
+}
+
+const LinkTag = ({ tag }) => {
+  const color = tagColors[tag.color]
+
+  return (
+    <div className='text-xs text-white text-center px-2 py-1 mr-2 rounded-full' style={{ background: color, minWidth: 30 }}>
+      {tag.name}
+    </div>
+  )
+}
+
+export default LinkTag
